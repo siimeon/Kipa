341af509c5c2c8b715a71f3f1d8e4c25bd7b6750
siimeon <simo.haakana@aalto.fi>
2015-04-06 08:42:45 +0000

    Updating Kipa project from Django 1.3 to Django 1.4 compatible

--- web/settings.py
+++ web/settings.py
@@ -28,12 +28,15 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+"""
 DATABASE_ENGINE = 'django.db.backends.sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
 DATABASE_NAME = hakemisto + '/tupa.db'     # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+"""
+DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': hakemisto + '/tupa.db' , 'USER': '', 'PASSWORD': '', 'HOST': '', 'PORT': ''}}
 
 # Cache
 TAUSTALASKENTA = False # Tulokset lasketaan taustalla (Vaatii toimiakseen tomivan cachekokoonpanon)
@@ -81,8 +84,8 @@ SECRET_KEY = 'shbtq($_^om(xep=5f97k2+ntb3!cqn+)%8r#s6udzqnhj$5p6'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.load_template_source',
 )
 
